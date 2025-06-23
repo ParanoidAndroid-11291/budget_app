97aae579a1cf19422d92c267afb0f08692cfcd25
Cody Hager <73755041+ParanoidAndroid-11291@users.noreply.github.com>
2025-06-22 20:16:12 -0400

    Testing transaction API with chart on root index page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,37 @@
-import { Handlers, FreshContext } from "$fresh/server.ts";
+import { Handlers, FreshContext, PageProps } from "$fresh/server.ts";
 import { State } from "./_middleware.ts";
 import Chart from "../islands/Chart.tsx";
+import moment from "moment"
+import { ZTransaction } from "../deno_kv/schemas.ts";
+import { ApiResponse } from "./api/_api_schemas.ts";
+import { z } from "zod/v4"
+
+type ApiResponse = z.infer<typeof ApiResponse>
+type Transaction = z.infer<typeof ZTransaction>
 
 export const handler: Handlers<any,State> = {
-  GET(_req: Request, ctx: FreshContext<State>) {
-    return ctx.render()
+  async GET(req: Request, ctx: FreshContext<State>) {
+    const url = new URL("/api/transactions", req.url)
+    url.searchParams.set("userId","01JX8XE6G424T2C8KX27CDJAYK")
+    const date = moment("2025-06-17").format("YYYY-MM-DD").toString()
+    url.searchParams.set("startDate",date)
+
+    const res = await fetch(url)
+    const data = await res.json() as ApiResponse
+
+    if (!data.success) return ctx.renderNotFound({
+      message: "Could not get transactions"
+    })
+
+    return ctx.render(data.data)
   }
 }
 
-export default function Home() {
+export default function Home(props: PageProps<Array<Transaction>>) {
   return (
     <div class="w-screen px-10 py-5 flex flex-col">
       <div class="w-full h-fit px-8">
-          <Chart />
+          <Chart data={props.data}/>
       </div>
       <div class="w-full h-5 my-5 bg-slate-400">
 
