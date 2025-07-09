58833eceb7aa558c35411b4859d2a1a390a5366c
Cody Hager <73755041+ParanoidAndroid-11291@users.noreply.github.com>
2025-07-09 11:06:06 -0400

    Added button styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,46 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import { z } from "zod/v4"
 
-export default (props: JSX.HTMLAttributes<HTMLButtonElement>) => {
-  return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-    />
-  );
+const ColorStyle = z.enum([
+  "primary",
+  "secondary",
+  "neutral",
+  "danger",
+  "warning",
+  "success",
+  "info"
+])
+
+type ColorStyle = z.infer<typeof ColorStyle>
+
+const ButtonStyle = z.enum(["solid","inverted"])
+type ButtonStyle = z.infer<typeof ButtonStyle>
+
+interface ButtonProps extends JSX.HTMLAttributes<HTMLButtonElement> {
+  colorStyle: ColorStyle,
+  buttonStyle: ButtonStyle
+}
+
+
+export default (props: ButtonProps) => {
+  
+  switch (props.colorStyle) {
+    case ColorStyle.enum.primary:
+      if (props.buttonStyle === "solid") return <button {...props}
+        disabled={!IS_BROWSER || props.disabled} 
+        className="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors" 
+      />
+
+      else return <button {...props}
+        disabled={!IS_BROWSER || props.disabled} 
+        className="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors" 
+        />
+
+    default:
+      return <button {...props}
+        disabled={!IS_BROWSER || props.disabled} 
+        className="border border-solid border-transparent rounded px-6 pb-1 pt-1.5 bg-carbon text-white" />
+  }
+
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,22 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        purp: "#AF05FF",
+        minty: "#00FFAB",
+        carbon: "#1E1E1E",
+        notWhite: "#EFEFEF",
+        stormy: "#666666",
+        chalkboard: "#333333",
+        foggy: "#E0E0E0",
+        danger: "#FF1E0A",
+        warning: "#FF9F05",
+        success: "#42CF00",
+        pinky: "#D67EFF",
+        pea: "#7ECCB2"
+      }
+    }
+  }
 } satisfies Config;
