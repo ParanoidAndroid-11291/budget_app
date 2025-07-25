254bd47ef7ff1705024bceb2b08957bef074a752
Cody Hager <73755041+ParanoidAndroid-11291@users.noreply.github.com>
2025-07-25 16:24:38 -0400

    better styling

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -3,9 +3,10 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export default (props: JSX.HTMLAttributes<HTMLInputElement>) => {
     return (
-        <div>
-            <input type="date" id="start_date"/>
-            <input type="date" id="end_date" />
+        <div class="flex justify-evenly items-center w-1/3 py-1.5 border rounded-full">
+            <input type="date" name="start" id="start" />
+            <span>-</span>
+            <input type="date" name="end" id="end" />
         </div>
     )
 }
